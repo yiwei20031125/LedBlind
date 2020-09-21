99d8eae3e3c48ab2ae82b9c27959686b96423c10
ganni <ghostgodgame@gmail.com>
2020-09-21 16:25:18 +0800

    又變得更久了

--- LedBlink.ino
+++ LedBlink.ino
@@ -6,7 +6,7 @@ pinMode(13, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(13,HIGH);
-delay(2000);
+delay(5000);
 digitalWrite(13,LOW);
-delay(2000);
+delay(5000);
 } 
